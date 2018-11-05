4687714c2c31e289eea65944c5633a76ab3ff022
shulzhenkoas <shulzhenkoas@gmail.com>
2018-11-05 18:26:43 +0300

    Лекция 3, задание №7

--- Lesson3_task7.py
+++ Lesson3_task7.py
@@ -4,3 +4,17 @@
 # Задача 7.
 # В одномерном массиве целых чисел определить два наименьших элемента. Они могут быть как равны между собой
 # (оба являться минимальными), так и различаться.
+
+import random
+
+AMOUNT = 20
+
+mass_elem = [random.randint(1, 100) for _ in range(AMOUNT)]
+print('Массив элементов:\n{}'.format(mass_elem))
+#print(sorted(mass_elem))
+
+for m in [0, 1]:
+    for n in range(m, len(mass_elem)):
+        if mass_elem[m] > mass_elem[n]:
+            mass_elem[m], mass_elem[n] = mass_elem[n], mass_elem[m]
+print('Два наименьшиз элемента массива "{}" и "{}"'.format(mass_elem[0], mass_elem[1]))
