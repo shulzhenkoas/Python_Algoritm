6bc52a75b72375e9201f4a0a6ee2344f2b0f22ea
shulzhenkoas <shulzhenkoas@gmail.com>
2018-11-08 16:46:07 +0300

    Лекция 4, задание №1

--- Lesson4_task1.py
+++ Lesson4_task1.py
@@ -9,16 +9,13 @@
 # Примечание: попробуйте написать несколько реализаций алгоритма и сравнить их.
 
 import cProfile
-#import random
-
+### Решение из домашнего задания
 def massivius(amount):
     import random
     mass_elem = [random.randint(1, 100) for _ in range(amount)]
     return mass_elem
 
 def minimax(amount):
-#    import random
-#    mass_elem = [random.randint(1, 100) for _ in range(amount)]
     mass_elem = massivius(amount)
     idx_min = idx_max = 0
 
@@ -47,4 +44,40 @@ def minimax(amount):
 # 1    0.000    0.000    0.043    0.043 Lesson4_task1.py:19(minimax)
 # 6498 function calls (6470 primitive calls) in 0.043 seconds
 
+# Время замены мест минимального и максимального элементов равно
+# времени его генерации. Не понимаю почему timeit показывает одно время
+# с cProfile больше.
+
+### С использованием встоенных функций из лекции №3
+def integrated(amount):
+    mass_elem = massivius(amount)
+    min_num = min(mass_elem)
+    max_num = max(mass_elem)
+    idx_min = mass_elem.index(min_num)
+    idx_max = mass_elem.index(max_num)
+    mass_elem[idx_min], mass_elem[idx_max] = mass_elem[idx_max], mass_elem[idx_min]
+    return(idx_min, idx_max, mass_elem)
+
+# Lesson4_task1.integrated(20)
+# 1000 loops, best of 3: 48.6 usec per loop
+# Lesson4_task1.integrated(100)
+# 1000 loops, best of 3: 224 usec per loop
+# Lesson4_task1.integrated(1000)
+# 1000 loops, best of 3: 2.21 msec per loop
+
+#cProfile.run('integrated(20)')
+# 1    0.000    0.000    0.027    0.027 Lesson4_task1.py:51(integrated)
+# 1289 function calls (1261 primitive calls) in 0.027 seconds
+#cProfile.run('integrated(100)')
+# 1    0.000    0.000    0.027    0.027 Lesson4_task1.py:51(integrated)
+# 1718 function calls (1690 primitive calls) in 0.027 seconds
+#cProfile.run('integrated(1000)')
+# 1    0.000    0.000    0.026    0.026 Lesson4_task1.py:51(integrated)
+# 6455 function calls (6427 primitive calls) in 0.026 seconds
+
+# cProfile показывает, что время выполнения функции замены мест минимального
+# и максимального элементов почему-то не растет с увеличением количества
+# элементов массива
+
 #print(*minimax(20), sep='\n')
+#print(*integrated(20), sep='\n')
